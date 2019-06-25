438e071b8deabd94c3eed4a3811166f78ca8a736
Marius Mézerette <marius.mez@gmail.com>
2019-06-25 08:55:00 +0200

    Specifiy to use rear camera if available

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,7 @@ export class AppComponent implements OnInit {
             .getUserMedia({
                 audio: false,
                 video: {
-                    facingMode: 'user',
+                    facingMode: 'environment',
                 }
             })
             .then(stream => {
